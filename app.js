09fc456daa00a64b379a2b251749b5106cfd0861
Priyanka Prajapati <priyanka@vebuin.com>
2023-10-03 13:49:04 +0530

    react-basics

---
+++ app.js
@@ -0,0 +1,34 @@
+//  Hello from React
+// const heading = React.createElement("h1", {id:"heading"}, "Hello from React");
+// const root = ReactDOM.createRoot(document.getElementById("root")) /* here we have created a root where we render our all react elements. This is done by ReactDOM */
+// console.log(heading)
+// root.render(heading)
+
+// creating nested object like below in react;
+
+// <div id="grandparent">
+//     <div id="parent">
+//         <h1>Hello I am the child</h1>
+//     </div>
+// </div>
+
+// const grandParent = React.createElement(
+//   "div",
+//   { id: "grandparent" },
+//   React.createElement(
+//     "div",
+//     { id: "parent" },
+//     React.createElement("h1", {}, "Hello I am the child")
+//   )
+// );
+
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+
+// console.log(grandParent);
+// root.render(grandParent);
+
+// React.createElement will return an object.
+// It is the "render" method which paints React Element as HTML element in the browser.
+
+
+
