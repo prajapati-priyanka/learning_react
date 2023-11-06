ccffff8751e056c04c8ef2515fa7dac464aea44e
Priyanka Prajapati <priyanka@vebuin.com>
2023-11-07 00:33:33 +0530

    Feat: Modified files

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
 //  Hello from React
 // const heading = React.createElement("h1", {id:"heading"}, "Hello from React");
 // const root = ReactDOM.createRoot(document.getElementById("root")) /* here we have created a root where we render our all react elements. This is done by ReactDOM */
@@ -12,20 +14,25 @@
 //     </div>
 // </div>
 
-// const grandParent = React.createElement(
-//   "div",
-//   { id: "grandparent" },
-//   React.createElement(
-//     "div",
-//     { id: "parent" },
-//     React.createElement("h1", {}, "Hello I am the child")
-//   )
-// );
+const grandParent = React.createElement(
+  "div",
+  { id: "grandparent" },
+ [React.createElement(
+    "div",
+    { id: "parent" },
+    React.createElement("h1", {}, "Hello I am the child")
+  )],
+ [React.createElement(
+    "div",
+    { id: "parent" },
+    React.createElement("h1", {}, "Hello h2 tag")
+  )],
+);
 
-// const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
 // console.log(grandParent);
-// root.render(grandParent);
+root.render(grandParent);
 
 // React.createElement will return an object.
 // It is the "render" method which paints React Element as HTML element in the browser.
